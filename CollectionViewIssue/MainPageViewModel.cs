dab2b8939047aac831193753aa1293e153eb9ee6
Aga Francis Anciano <aaa@1902.software>
2019-09-25 04:10:58 -0700

    CollectionViewIssue

---
+++ CollectionViewIssue/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CollectionViewIssue
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            PropertyChanged += MainPageViewModel_PropertyChanged;
+            Images = new List<string>
+            {
+                "bg_blue",
+                "bg_brown",
+                "bg_green",
+                "bg_grey",
+                "bg_pink",
+                "bg_purple",
+                "bg_yellow",
+            };
+        }
+
+        private void MainPageViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<string> Images { get; set; }
+    }
+}
